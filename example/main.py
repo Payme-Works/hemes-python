0886b5a2feaf55d3d5abe9afaa789f2e2ba3c1fb
André 'Dezzy <andrevcv1@gmail.com>
2020-10-21 00:23:14 -0300

    chore: Update example

--- example/main.py
+++ example/main.py
@@ -5,9 +5,12 @@ import os
 from hermes.stable_api import StableHermes as Hermes
 
 account = Hermes(os.getenv('EMAIL'), os.getenv('PASSWORD'))
+account.connect()
 
 balances = account.get_balances()
-print(balances)
+real_balance = next((item for item in balances if item['type'] == 1), None)
+
+print('Real balance: {}\n'.format(real_balance['amount']))
 
 account.change_balance('PRACTICE')
 
