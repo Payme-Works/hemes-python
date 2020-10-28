03b3692508aef21473cb5646154fafdd584fb93f
André 'Dezzy <andrevcv1@gmail.com>
2020-10-28 13:10:13 -0300

    chore: Update example

--- example/main.py
+++ example/main.py
@@ -16,25 +16,22 @@ print(f'Real balance: {real_balance["amount"]}\n')
 
 hermes.change_balance('practice')
 
-# _, order_id = hermes.buy({
-#     'price_amount': 2,
-#     'active': 'EURUSD',
-#     'action': 'put',
-#     'expiration': 1
-# })
-# print(order_id)
+_, order_id = hermes.buy({
+    'type': 'digital',
+    'active': 'EURUSD',
+    'price_amount': 2,
+    'action': 'put',
+    'expiration': 1
+})
+print(order_id)
 
 # all_open_actives = hermes.get_all_open_actives()
 # print(json.dumps(all_open_actives))
 
 active = 'EURUSD'
 
-candles = hermes.get_candles(active, '5M')
-print(json.dumps(candles[len(candles) - 5]))
-print(json.dumps(candles[len(candles) - 4]))
-print(json.dumps(candles[len(candles) - 3]))
-print(json.dumps(candles[len(candles) - 2]))
-print(json.dumps(candles[len(candles) - 1]))
+order = hermes.wait_for_result(order_id)
+print(order)
 
 trend = hermes.get_trend(active, '5M')
 print(f'\n[{active}] Trend: {trend}')
